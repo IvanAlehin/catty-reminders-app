b462c413a215ec9b44d02b138c50c930d1b81a77
IvanAlehin <ivanalehin236@gmail.com>
2025-10-29 19:57:21 +0000

    test: update login page

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Начинаем развертывание приложения..."
-
-source /home/ivan/app/venv/bin/activate
-pip install -r ./requirements.txt
-
-sudo systemctl restart myapp.service
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Running tests..."
-
-source /home/ivan/app/venv/bin/activate
-pip install -r ./requirements.txt
-pip install playwright
-playwright install --with-deps chromium
-python3 -m pytest
